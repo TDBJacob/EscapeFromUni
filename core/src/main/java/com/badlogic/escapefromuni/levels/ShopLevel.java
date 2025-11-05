0bfbb38daec40287bd01403a718d6fd8a632c3dc
HJAV21 <huzaifajavaid111111@gmail.com>
2025-11-05 23:18:31 +0000

    3

--- core/src/main/java/com/badlogic/escapefromuni/levels/ShopLevel.java
+++ core/src/main/java/com/badlogic/escapefromuni/levels/ShopLevel.java
@@ -1,7 +1,5 @@
 package com.badlogic.escapefromuni.levels;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 public class ShopLevel extends Level{
 
     public ShopLevel() {
@@ -10,19 +8,6 @@ public class ShopLevel extends Level{
         sideX = 28;
         sideY = 15;
 
-    }
-
-    @Override
-    public void update(float deltaTime) {
-    }
-
-    @Override
-    public void draw(SpriteBatch batch) {
-    }
-
-    @Override
-    public boolean collides(com.badlogic.gdx.math.Rectangle rectangle) {
-        return false;
     }
 
 }
